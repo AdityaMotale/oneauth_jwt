750ec531c15b44f5c8a0ec7a8185148224244852
Aditya <77727680+AdityaMotale@users.noreply.github.com>
2022-09-14 16:31:19 +0530

    added new get request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Get} from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
+
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getUsername(): string {
+    return this.appService.getUsername();
+  }
+
+  @Post()
+  postUsername(@Body() body): any {
+    return {
+      name: body.name,
+      data: "User name created",
+    };
   }
 }
